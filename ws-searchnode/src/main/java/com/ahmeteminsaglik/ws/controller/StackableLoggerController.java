8d3b3035c1eb93fdcf4544fb941554b332e77dd9
Ahmet Emin SAĞLIK <ahmeteminsaglik@gmail.com>
2024-05-05 03:53:44 +0300

    Error is fixed using @CrossOrigin

--- ws-searchnode/src/main/java/com/ahmeteminsaglik/ws/controller/StackableLoggerController.java
+++ ws-searchnode/src/main/java/com/ahmeteminsaglik/ws/controller/StackableLoggerController.java
@@ -2,15 +2,13 @@ package com.ahmeteminsaglik.ws.controller;
 
 import com.ahmeteminsaglik.ws.business.abstracts.StackableLogger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/log")
+@CrossOrigin
 public class StackableLoggerController {
 
     private StackableLogger stackableLogger;
